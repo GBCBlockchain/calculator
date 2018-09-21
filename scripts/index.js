d83185b7ea32453e624b0fb84a065b1181279f24
dylanbaker21 <dylan.j.baker21@hotmail.com>
2018-09-20 21:44:07 -0400

    if/else statement with functions

--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,15 @@ function division(x,y) {
     var z = x / y
     console.log(z)
 }
+
+var sum = addition(10,12)
+var dif = subtraction(10,8) 
+var prod = multiplication(6,3)
+var div = division(100,5)
+
+if (sum>dif) {
+    console.log("the sum is greater than the difference")
+}
+else {
+    console.log("the difference is greater than the sum")
+}
