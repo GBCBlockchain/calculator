9fa692ae10a3a69454c2589a69ecb0390bf39b64
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-09-20 21:07:54 -0400

    Add console prints.

--- adam-script.js
+++ adam-script.js
@@ -17,3 +17,9 @@ function divide (num1, num2) {
     var z = num1 / num2;
     return z;
 }
+
+console.log(add(1, 2));
+console.log(subtract(2,1));
+console.log(multiply(2,2));
+console.log(divide(4,2));
+
