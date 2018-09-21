3f102a8ec5e2c265ad0306fdcfb236859cccc1c7
priom <priom.galaxy@gmail.com>
2018-09-20 21:46:33 -0400

    variables, functions, conditions

---
+++ scripts/calculator.js
@@ -0,0 +1,28 @@
+function add(x, y) {
+    var z = x + y
+    return z
+}
+
+function sub(x, y) {
+    var z = x - y
+    return z
+}
+
+function mul(x, y) {
+    var z = x * y
+    return z
+}
+
+function div(x, y) {
+    var z = x / y
+    return z
+}
+
+var sum = add(11, 22)
+console.log(sum)
+
+console.log(sub(44, 2))
+console.log(mul(5, 6))
+
+const div2 = div(40, 5)
+console.log(div2)
