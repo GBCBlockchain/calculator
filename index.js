64e90a35b3f4362d2b233f9d2ded6e8f987cb803
dylanbaker21 <dylan.j.baker21@hotmail.com>
2018-09-20 21:02:55 -0400

    created 4 math functions

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function addition(x,y) {
+    var z = x + y
+    console.log(z)
+}
+
+function subtraction(x,y) {
+    var z = x - y
+    console.log(z)
+}
+
+function multiplication(x,y) {
+    var z = x * y
+    console.log(z)
+}
+
+function division(x,y) {
+    var z = x / y
+    console.log(z)
+}
